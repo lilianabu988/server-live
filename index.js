5395a1d419c095cdf7a82987af96405c571e4715
lilianabu988 <127079820+lilianabu988@users.noreply.github.com>
2023-03-07 07:31:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'http://localhost:5500',
-    'http://127.0.0.1:5500',
+    'https://livedeviceconnect.onrender.com',
+//     'http://127.0.0.1:5500',
 ]
 const corsOptions = {
     origin: (origin, callback) => {
